19a8e96628020de9ec7b985fb8189704bd4d7cf5
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-08-10 13:32:21 +0300

    topbar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const cars = JSON.parse(DATA);
 const showcaseEl = document.getElementById("showcase");
-
+const sortSelectEl = document.getElementById('sortSelect')
+const searchFormEl = document.getElementById('searchForm')
 // {
 //     "id": "89aed5b8c686ebd713a62873e4cd756abab7a106",
 //     "make": "BMW",
@@ -26,6 +27,49 @@ const showcaseEl = document.getElementById("showcase");
 //   }
 
 
+searchFormEl.addEventListener('submit', event => {
+  event.preventDefault()
+  console.log(event.target);
+  console.log(event.target.search);
+  console.log(event.target.search.value);
+  const query = event.target.search.value.trim().toLowerCase().split(' ').filter(word => !!word)
+
+  console.log(query);
+})
+
+
+
+sortSelectEl.addEventListener('change', event => {
+  const [key, order] = event.target.value.split('/')
+  if (typeof cars[0][key] === 'string') {
+    cars.sort((a, b) => {
+      return (a[key].localeCompare(b[key])) * order
+    })
+  } else {
+    cars.sort((a, b) => {
+      return (a[key] - b[key]) * order
+    })
+  }
+  // if (sortingType === 'price-high-low') {
+  //   cars.sort((a, b) => {
+  //     return b.price - a.price
+  //   })
+  // } else if (sortingType === 'price-low-high') {
+  //   cars.sort((a, b) => {
+  //     return a.price - b.price
+  //   })
+  // } else if (sortingType === 'year-high-low') {
+  //   cars.sort((a, b) => {
+  //     return b.year - a.year
+  //   })
+  // } else if (sortingType === 'year-low-high') {
+  //   cars.sort((a, b) => {
+  //     return a.year - b.year
+  //   })
+  // }
+  renderCardShowcase(cars, showcaseEl);
+})
+
 renderCardShowcase(cars, showcaseEl);
 
 function renderCardShowcase(cardsDataArr, cardShowcaseEl) {
@@ -41,36 +85,32 @@ function createCardHTML(cardDataObj) {
   for (let i = 0; i < 5; i++) {
     if (cardDataObj.rating > i + 0.5) {
       starsHTML += `<i class="fas fa-star"></i>`;
-    } 
+    }
     else if (cardDataObj.rating == i + 0.5) {
       starsHTML += `<i class="fas fa-star-half-alt"></i>`;
-    } 
+    }
     else {
       starsHTML += `<i class="far fa-star"></i>`;
     }
   }
-  
+
   return `<div class="card">
 
   
-    <img class="card__img" src="${cardDataObj.img}" alt="${cardDataObj.make} ${
-    cardDataObj.model
-  } ${cardDataObj.engine_volume}L (${
-    cardDataObj.year
-  })" width="1" height="1" loading="lazy" decoding="async">
+    <img class="card__img" src="${cardDataObj.img}" alt="${cardDataObj.make} ${cardDataObj.model
+    } ${cardDataObj.engine_volume}L (${cardDataObj.year
+    })" width="1" height="1" loading="lazy" decoding="async">
 
   <div class="card-body-top">
-  ${
-    cardDataObj.top
+  ${cardDataObj.top
       ? `<p class="card__top ${cardDataObj.top ? `top` : `untop`}">Топ ${cardDataObj.top ? `<i class="fas fa-certificate"></i>` : ``}</p>`
       : ""
-  }
+    }
 </div>
 
     <div class="card__body">
-        <h2 class="card-body__title">${cardDataObj.make} ${cardDataObj.model} ${
-    cardDataObj.year
-  } г.</h2>
+        <h2 class="card-body__title">${cardDataObj.make} ${cardDataObj.model} ${cardDataObj.year
+    } г.</h2>
         <h3 class="card-body__price">${cardDataObj.price} $</h3>
         <h4 class="card-body__rating">${starsHTML} </h4>
         <dl class="card__characteristics">
@@ -121,56 +161,53 @@ function createCardHTML(cardDataObj) {
               </div>
               </dl>
           </div>
-        ${
-          cardDataObj.vin
-            ? `<h4 class="card-body__vin ${cardDataObj.vin_check ? `checked` : `unchecked`}">${cardDataObj.vin_check ? `<i class="fas fa-check-circle"></i>` : `<i class="fas fa-times-circle"></i>`} Vin-код: ${cardDataObj.vin}</h4>`
-            : ""
-        }
+        ${cardDataObj.vin
+      ? `<h4 class="card-body__vin ${cardDataObj.vin_check ? `checked` : `unchecked`}">${cardDataObj.vin_check ? `<i class="fas fa-check-circle"></i>` : `<i class="fas fa-times-circle"></i>`} Vin-код: ${cardDataObj.vin}</h4>`
+      : ""
+    }
         <div class="card-body__stats">
         <p class="card-body__timestamp">${new Date(
-          cardDataObj.timestamp
-        ).toLocaleDateString()}</p>
-        <p class="card-body__views">Просмотров: ${
-          cardDataObj.views}</p>
+      cardDataObj.timestamp
+    ).toLocaleDateString()}</p>
+        <p class="card-body__views">Просмотров: ${cardDataObj.views}</p>
           </div>
     </div>
 </div>`;
 }
 
-//  const nums = [1,2,3]
-
-//  console.log(`${nums}`);
-
-// const incrementedNums = nums.map(num => `<h2>${num}</h2>`)
-
-// console.log(nums, incrementedNums);
-
-// <a href="tel:${cardDataObj.phone}" class="card-body__link">Call to seller</a>
 
 
+// const arr = [2,1,3,12,15,28,6,7,90]
+// const arr = [{ name: 'Ivan' }, { name: 'Andrey' }, { name: 'David' }, { name: 'Aleksey' }, { name: 'Katerina' }, { name: 'Yulia' }, { name: 'Anton' }, { name: 'Oleg' }]
+// console.log(arr);
 
-// document.addEventListener('click', () => {
-//   console.log('document Click');
+// arr.sort((a, b) => {
+//   return a.name.localeCompare(b.name)
 // })
 
-showcaseEl.addEventListener('click', (event) => {
-  const imageEl = event.target.closest('.card__img')
-  if (imageEl) {
-    console.log('Image click');
-  }
-})
+// console.log(arr);
 
-// const cardImages = document.querySelectorAll('.card__img')
-// console.log(cardImages);
-// cardImages.forEach((image) => {
-//   image.addEventListener('click', () => {
-//     console.log('Click on image');
-//   })
+
+// a - b === (a - b) * 1
+
+// b - a === (a - b) * -1
+
+
+// const numbers = [1,12,56,12,78,32,45,1,2,3,2,6,12]
+
+// // const filteredNumbers = numbers.filter(number => {
+// //   return number > 10
+// // })
+// console.log(numbers);
+// // console.log(filteredNumbers);
+
+// const moreThanZero = numbers.every(number => {
+//   console.log(number);
+//   return number > 0
+// })
+// const moreThanZero = numbers.some(number => {
+//   console.log(number);
+//   return number > 0
 // })
 
-const formEl = document.getElementById('form')
-
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  console.log('Submit');
-})
+// console.log(moreThanZero);
